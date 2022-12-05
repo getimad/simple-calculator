7a2633250ac5e45fcdedd1d0607b1cde713172bd
getimad <getimad.main@gmail.com>
2022-12-05 21:39:07 +0100

    Modify the usage message and use functions
    - Modify the usage message and add exit command in the usage function.
    - Add two functions (main and usage).

--- main.sh
+++ main.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 3 ]; then
-    case $2 in
-        -a|--add|+)
-            RESULT=$(($1 + $3));;
-        -s|--sub|-)
-            RESULT=$(($1 - $3));;
-        -m|--mul|x)
-            RESULT=$(($1 * $3));;
-        -d|--div|/)
-            RESULT=$(($1 / $3));;
-    esac
-    echo $* = $RESULT
-else
-    echo "ERROR: The script expect only 3 arguments."
-fi
+usage() {
+    echo "Usage: ${0##/*} [number] [operator | --ope | -o] [number]"
+    exit 1
+}
+
+main() {
+    if [ $# -eq 3 ]; then
+        case $2 in
+            -a|--add|+)
+                RESULT=$(($1 + $3));;
+            -s|--sub|-)
+                RESULT=$(($1 - $3));;
+            -m|--mul|x)
+                RESULT=$(($1 * $3));;
+            -d|--div|/)
+                RESULT=$(($1 / $3));;
+        esac
+        echo $* = $RESULT
+    else
+        usage
+    fi
+}
+
+main "$@"
